2aa636b168b328749a6fe0bbc4298d23fdfeb0b0
Pokifi <109711670+Pokifi@users.noreply.github.com>
2023-10-13 01:34:16 -0300

    Create Lkpainel.py

---
+++ Lkpainel.py
@@ -0,0 +1,181 @@
+import os
+import time
+print("Bem vindo ao Painel Lk!")
+time.sleep(1)
+print("Carregando...")  
+time.sleep(1)
+os.system("clear")
+print("Carregando...")
+time.sleep(1)
+os.system("clear")
+print("Carregando...")
+time.sleep(1)
+os.system("clear")
+time.sleep(1)
+print("|炉炉|    '鈥檤炉炉|/炉炉/      |炉炉|\炉炉\   /鈥橽    |炉炉||炉炉 \|炉炉||炉炉|\炉炉\ |炉炉|    '鈥�")
+print("|   '|__ 鈥檤   '|   '(       |   '|/__/ '/ o '\  |   '||   '|\    '||   鈥�'>炉_ |   '|__ 鈥�")
+print("|_____||__|\__\      |__|     '/____\鈥檤__||__|鈥�'|__||__|/__/ |_____|")
+print("LK PAINEL")
+
+print("1: Atualizar Termux")
+print("2: Instalar UniTools")
+print("3: Apks Devs")
+print("4: Gerar Ccs")
+print("5: Tww Grupo")
+print("6: Sair")
+
+escolha = False
+while escolha == False:
+ nivel = int(input("Qual op莽茫o: "))
+    
+ if (nivel == 1):
+    os.system("pkg update && pkg upgrade")
+    
+ elif (nivel == 2):
+     os.system("pkg install git")
+     os.system("pkg install python")
+     os.system("git clone https://github.com/Zian25/UniTools-Termux")
+     os.system("cd UniTools-Termux")
+     os.system("python3 utx.py")
+    
+ elif (nivel == 3):
+      os.system("clear")
+      print("Bem vindo ao Apks do Lk!")
+      time.sleep(1)
+      print("Carregando...")
+      time.sleep(2)
+      os.system("clear")
+      print("|炉炉|    '鈥檤炉炉|/炉炉/      |炉炉|\炉炉\   /鈥橽    |炉炉||炉炉 \|炉炉||炉炉|\炉炉\ |炉炉|    '鈥�")
+      print("|   '|__ 鈥檤   '|   '(       |   '|/__/ '/ o '\  |   '||   '|\    '||   鈥�'>炉_ |   '|__ 鈥�")
+      print("|_____||__|\__\      |__|     '/____\鈥檤__||__|鈥�'|__||__|/__/ |_____|")
+      print("LK APKS")
+
+      print("1: Mt Manager")
+      print("2: Apk Encriptor")
+      print("3: Devs Tools")
+      print("4: Ads Regex")
+      print("5: Sair")
+      print("6: Retornar")
+
+ escolha = False
+ while escolha == False:
+   nivel = int(input("Qual op莽茫o: "))
+    
+   if (nivel == 1):
+        print("https://drive.google.com/u/0/uc?id=1FKHxk1jW1laWDdMKNspAlBCGSNdmW1KM&export=download")
+    
+   elif (nivel == 2):
+        print("https://drive.google.com/u/0/uc?id=1EXnPQqdt7sCk1Er7kqNToHBZXbV0VPIV&export=download")
+        
+   elif (nivel == 3):
+        print("https://drive.google.com/u/0/uc?id=19QyV9w_GHthoCtm6LrqxRCA7rxF0gRYY&export=download")
+    
+   elif (nivel == 4):
+         print("https://drive.google.com/u/0/uc?id=1kB7osUHoqrfzTPUpAHUtKqTqjKOU0qPH&export=download")
+         
+   elif (nivel == 5):
+         exit()
+   
+   elif (nivel == 6):
+     import os
+     import time
+     print("Bem vindo ao Painel Lk!")
+     time.sleep(1)
+     print("Carregando...")  
+     time.sleep(1)
+     os.system("clear")
+     print("Carregando...")
+     time.sleep(1)
+     os.system("clear")
+     print("Carregando...")
+     time.sleep(1)
+     os.system("clear")
+     time.sleep(1)
+     print("|炉炉|    '鈥檤炉炉|/炉炉/      |炉炉|\炉炉\   /鈥橽    |炉炉||炉炉 \|炉炉||炉炉|\炉炉\ |炉炉|    '鈥�")
+     print("|   '|__ 鈥檤   '|   '(       |   '|/__/ '/ o '\  |   '||   '|\    '||   鈥�'>炉_ |   '|__ 鈥�")
+     print("|_____||__|\__\      |__|     '/____\鈥檤__||__|鈥�'|__||__|/__/ |_____|")
+     print("LK PAINEL")
+
+     print("1: Atualizar Termux")
+     print("2: Instalar UniTools")
+     print("3: Apks Devs")
+     print("4: Gerar Ccs")
+     print("5: Tww Grupo")
+     print("6: Sair")
+
+ escolha = False
+ while escolha == False:
+   nivel = int(input("Qual op莽茫o: "))
+    
+   if (nivel == 1):
+     os.system("pkg update && pkg upgrade")
+    
+   elif (nivel == 2):
+     os.system("pkg install git")
+     os.system("pkg install python")
+     os.system("git clone https://github.com/Zian25/UniTools-Termux")
+     os.system("cd UniTools-Termux")
+     os.system("python3 utx.py")
+    
+   elif (nivel == 3):
+      os.system("clear")
+      print("Bem vindo ao Apks do Lk!")
+      time.sleep(1)
+      print("Carregando...")
+      time.sleep(2)
+      os.system("clear")
+      print("|炉炉|    '鈥檤炉炉|/炉炉/      |炉炉|\炉炉\   /鈥橽    |炉炉||炉炉 \|炉炉||炉炉|\炉炉\ |炉炉|    '鈥�")
+      print("|   '|__ 鈥檤   '|   '(       |   '|/__/ '/ o '\  |   '||   '|\    '||   鈥�'>炉_ |   '|__ 鈥�")
+      print("|_____||__|\__\      |__|     '/____\鈥檤__||__|鈥�'|__||__|/__/ |_____|")
+      print("LK APKS")
+
+      print("1: Mt Manager")
+      print("2: Apk Encriptor")
+      print("3: Devs Tools")
+      print("4: Ads Regex")
+      print("5: Sair")
+      print("6: Retornar")
+
+ escolha = False
+ while escolha == False:
+    nivel = int(input("Qual op莽茫o: "))
+    
+    if (nivel == 1):
+        print("https://drive.google.com/u/0/uc?id=1FKHxk1jW1laWDdMKNspAlBCGSNdmW1KM&export=download")
+    
+    elif (nivel == 2):
+        print("https://drive.google.com/u/0/uc?id=1EXnPQqdt7sCk1Er7kqNToHBZXbV0VPIV&export=download")
+        
+    elif (nivel == 3):
+        print("https://drive.google.com/u/0/uc?id=19QyV9w_GHthoCtm6LrqxRCA7rxF0gRYY&export=download")
+    
+    elif (nivel == 4):
+         print("https://drive.google.com/u/0/uc?id=1kB7osUHoqrfzTPUpAHUtKqTqjKOU0qPH&export=download")
+         
+    elif (nivel == 5):
+         exit()
+
+ 
+    elif (nivel == 4):
+     print("Gerando Ccs...")
+     time.sleep(3)
+     print("2054705767706646|11|2028|447")
+     print("Cc valido gerado com Sucesso!")
+    
+    elif (nivel == 5):
+      print("https://chat.whatsapp.com/FARzirAXEziGNHLF4wDeL5")
+        
+    elif (nivel == 6):
+     exit()
+ 
+    elif (nivel == 4):
+      print("Gerando Ccs...")
+      time.sleep(3)
+      print("2054705767706646|11|2028|447")
+      print("Cc valido gerado com Sucesso!")
+    
+    elif (nivel == 5):
+      print("https://chat.whatsapp.com/FARzirAXEziGNHLF4wDeL5")
+        
+    elif (nivel == 6):
+      exit()
